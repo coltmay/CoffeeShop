3bf386c134ad266520f5ddd9a27b252f341ccd68
ColtMay <colten_mayberry@pm.me>
2021-07-06 15:41:14 -0500

    added CoffeeController

--- Controllers/BeanVarietyController.cs
+++ Controllers/BeanVarietyController.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Mvc;
 using CoffeeShop.Models;
 using CoffeeShop.Repositories;
 
@@ -68,4 +63,3 @@ namespace CoffeeShop.Controllers
         }
     }
 }
-
