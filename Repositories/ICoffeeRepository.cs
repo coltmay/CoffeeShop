8d8b3b434ea1d8cf45aa204a37ae2b1c3a357bd2
ColtMay <colten_mayberry@pm.me>
2021-07-06 15:21:58 -0500

    CoffeeRepository interface extracted

---
+++ Repositories/ICoffeeRepository.cs
@@ -0,0 +1,16 @@
+﻿// Repository Provided By NSS
+
+using CoffeeShop.Models;
+using System.Collections.Generic;
+
+namespace CoffeeShop.Repositories
+{
+    public interface ICoffeeRepository
+    {
+        void Add(Coffee coffee);
+        void Delete(int id);
+        Coffee Get(int id);
+        List<Coffee> GetAll();
+        void Update(Coffee coffee);
+    }
+}
