02c9d4efd967884a9d90dd55c0ed0155d170c950
ColtMay <colten_mayberry@pm.me>
2021-07-06 13:35:35 -0500

    first commit

---
+++ Repositories/IBeanVarietyRepository.cs
@@ -0,0 +1,16 @@
+﻿// Repository Provided By NSS
+
+using CoffeeShop.Models;
+using System.Collections.Generic;
+
+namespace CoffeeShop.Repositories
+{
+    public interface IBeanVarietyRepository
+    {
+        void Add(BeanVariety variety);
+        void Delete(int id);
+        BeanVariety Get(int id);
+        List<BeanVariety> GetAll();
+        void Update(BeanVariety variety);
+    }
+}
---
+++ js-app/main.js
@@ -0,0 +1,15 @@
+﻿// Javascript Provided By NSS
+
+const url = "https://localhost:5001/api/beanvariety/";
+
+const button = document.querySelector("#run-button");
+button.addEventListener("click", () => {
+    getAllBeanVarieties()
+        .then(beanVarieties => {
+            console.log(beanVarieties);
+        })
+});
+
+function getAllBeanVarieties() {
+    return fetch(url).then(resp => resp.json());
+}
